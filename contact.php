da6a8f5555c40395a55941f4116658eeb83b3a21
Zubkowicz <103575710+Zubkowicz@users.noreply.github.com>
2022-04-13 16:18:04 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projekt 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        Contact
+    </header>
+
+    <div id="menu">
+    <a href="index.php"><button class="button button1">Home</button></a>
+    <a href="about_me.php"><button class="button button1">About me</button></a>
+    <a href="contact.php"><button class="button button1">Contact</button></a>
+    </div>
+
+
+    <div id="text">
+        <h2>Skontaktuj się ze mną:</h2> 
+
+            <form style="text-align:justify;">
+                From:       <input type="text" placeholder="e-mail or username"><br><br>
+                Question:   <input type="text" placeholder="question..."><br><br>
+
+                <button class="button2 button3">Send</button>
+            </form>
+            
+    </div>
+
+</body>
+</html>
