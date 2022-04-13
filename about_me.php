07e4df947758298f4923533c41a6bfa86b8ae66a
Zubkowicz <103575710+Zubkowicz@users.noreply.github.com>
2022-04-13 13:50:03 +0200

    Add files via upload

---
+++ about_me.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projekt 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        About me
+    </header>
+    <a href="index.php" id="a1"><button class="button button2">Home</button></a>
+    <a href="about_me.php" id="a2"><button class="button button2">About me</button></a>
+    <a href="contact.php" id="a3"><button class="button button2">Contact</button></a>
+    <div id="a4"></div>
+
+    <div id="b1"></div>
+    <div id="b2">
+        <h2>O mnie</h2>
+        <p style="text-align:justify;"> 
+            Nazywam się Gabrysia Zubkowicz. Mam 18 lat i chodzę do 3 klasy technikum informatycznego. 
+            Od urodzenia mieszkam pod Warszawą w miejcowości o nazwie Ząbki. Chodziłam tam do szkoły podstawowej i gimnazjum. 
+        </p>
+    </div>
+    <div id="b3"></div>
+</body>
+</html>
